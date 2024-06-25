844bd62fe114f77133a7beff43d5e2b5f6391a31
devdan <f.gomez@univermilenium.edu.mx>
2024-06-25 15:57:17 -0600

    Se agregan las clases iniciales al proyecto

---
+++ src/main/java/com/univer/estudiantes/controller/EstudianteController.java
@@ -0,0 +1,28 @@
+package com.univer.estudiantes.controller;
+
+import com.univer.estudiantes.entity.EstudianteEntity;
+import com.univer.estudiantes.repository.EstudianteRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class EstudianteController {
+
+    @Autowired
+    private EstudianteRepository repository;
+
+    @GetMapping("/api/univer/estudiante/{id}")
+    public EstudianteEntity estudiantePorId(@PathVariable Integer id){
+
+        if(repository.findById(id).isPresent()){
+            return repository.findById(id).get();
+        } else {
+            return new EstudianteEntity();
+        }
+
+    }
+
+
+}
---
+++ src/main/java/com/univer/estudiantes/entity/EstudianteEntity.java
@@ -0,0 +1,76 @@
+package com.univer.estudiantes.entity;
+
+import jakarta.persistence.*;
+
+@Entity
+@Table(name = "ESTUDIANTE")
+public class EstudianteEntity {
+
+    @Id
+    @Column(name = "ID")
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer idEstudiante;
+
+    @Column(name = "NOMBRE")
+    private String nombre;
+
+    @Column(name = "APELLIDO")
+    private String apellido;
+
+    @Column(name = "ENROLADO")
+    private Boolean enrolado;
+
+    @Column(name = "ID_CURSO")
+    private Integer idCurso;
+
+    @Column(name = "TELEFONO")
+    private Integer telefono;
+
+    public Integer getTelefono() {
+        return telefono;
+    }
+
+    public void setTelefono(Integer telefono) {
+        this.telefono = telefono;
+    }
+
+    public Integer getIdEstudiante() {
+        return idEstudiante;
+    }
+
+    public void setIdEstudiante(Integer idEstudiante) {
+        this.idEstudiante = idEstudiante;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getApellido() {
+        return apellido;
+    }
+
+    public void setApellido(String apellido) {
+        this.apellido = apellido;
+    }
+
+    public Boolean getEnrolado() {
+        return enrolado;
+    }
+
+    public void setEnrolado(Boolean enrolado) {
+        this.enrolado = enrolado;
+    }
+
+    public Integer getIdCurso() {
+        return idCurso;
+    }
+
+    public void setIdCurso(Integer idCurso) {
+        this.idCurso = idCurso;
+    }
+}
