6c389c70c315147eb4fcbef011fe6c9c6af72e0c
ABAD <1232100543@univermilenium.edu.mx>
2024-06-30 20:53:31 -0600

    Se agrega entregable2

---
+++ src/main/java/com/univer/estudiantes/controller/EstudianteRequest.java
@@ -0,0 +1,50 @@
+package com.univer.estudiantes.controller;
+
+public class EstudianteRequest {
+    private String nombre;
+    private String apellido;
+    private Boolean enrolado;
+    private Integer IdCurso;
+    private Integer telefono;
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getApellido() {
+        return apellido;
+
+    }
+
+    public void setApellido(String apellido) {
+        this.apellido = apellido;
+    }
+
+    public Boolean getEnrolado() {
+        return enrolado;
+    }
+
+    public void setEnrolado(Boolean enrolado) {
+        this.enrolado = enrolado;
+    }
+
+    public Integer getIdCurso() {
+        return IdCurso;
+    }
+
+    public void setIdCurso(Integer idCurso) {
+        IdCurso = idCurso;
+    }
+
+    public Integer getTelefono() {
+        return telefono;
+    }
+
+    public void setTelefono(Integer telefono) {
+        this.telefono = telefono;
+    }
+}
