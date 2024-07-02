e131fe0886606b284638e1a26cf1574d841c82ef
Jorge Al <1232100544@univermilenium.edu.mx>
2024-07-01 19:39:55 -0600

    CAMBIOS ACT 2

--- src/main/java/com/univer/estudiantes/controller/EstudianteController.java
+++ src/main/java/com/univer/estudiantes/controller/EstudianteController.java
@@ -3,9 +3,9 @@ package com.univer.estudiantes.controller;
 import com.univer.estudiantes.entity.EstudianteEntity;
 import com.univer.estudiantes.repository.EstudianteRepository;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 public class EstudianteController {
@@ -14,15 +14,26 @@ public class EstudianteController {
     private EstudianteRepository repository;
 
     @GetMapping("/api/univer/estudiante/{id}")
-    public EstudianteEntity estudiantePorId(@PathVariable Integer id){
-
-        if(repository.findById(id).isPresent()){
-            return repository.findById(id).get();
-        } else {
-            return new EstudianteEntity();
+    public ResponseEntity<EstudianteEntity> estudianteporId(@PathVariable Integer id){
+        if (repository.findById(id).isPresent()){
+            return new ResponseEntity<>(repository.findById(id).get(), HttpStatus.OK);
+        }else {
+            return new ResponseEntity<>(HttpStatus.NOT_FOUND);
         }
 
     }
+    @PostMapping("/api/univer/estudiante/save")
+    public ResponseEntity<EstudianteEntity> guardarEstudiante(@RequestBody EstudianteRequest request) {
+        EstudianteEntity estudiante = new EstudianteEntity();
+        estudiante.setNombre(request.getNombre());
+        estudiante.setApellido(request.getApellido());
+        estudiante.setEnrolado(request.getEnrolado());
+        estudiante.setIdCurso(request.getIdCurso());
+        estudiante.setTelefono(request.getTelefono());
 
+        EstudianteEntity savedEstudiante = repository.save(estudiante);
+
+        return new ResponseEntity<>(savedEstudiante, HttpStatus.CREATED);
+    }
 
 }
