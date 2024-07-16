48a87539915cb79ac117ac83cbb0d54303ce79cf
REDZR03 <resendiznoe123@gmail.com>
2024-07-15 22:28:16 -0600

    Avance para Producto final 15 de julio creacion del fetch para estudiantes

---
+++ src/main/resources/GP/fetch_alumnos.js
@@ -0,0 +1,26 @@
+const estudiante ={
+    nombre: "Noe",
+    apellido:"Resendiz",
+    idCurso:1
+}
+
+fetch('http://localhost:8686/api/univer/estudiante/save',{
+    method: "POST",
+    body: JSON.stringify(estudiante),
+    headers: {"Content-type":"application/json"}
+})
+.then(response =>{
+    console.log(response)
+    if(response.ok && response.status===201){
+        console.log('dentro de la primera promesa')
+        return response.json()
+    }else{
+        throw Error (response.statusText)
+    }
+})
+.then(json=>{
+    console.log('segunda promesa')
+    console.log(json)
+    return json;
+})
+.catch(error=>console.log(error))
