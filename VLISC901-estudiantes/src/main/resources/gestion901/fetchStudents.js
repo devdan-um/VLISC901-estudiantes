349bbb85e217aab62359b109ed7b677e662ec76c
Ismael <iarsfate@gmail.com>
2024-07-15 22:18:51 -0600

    creacion del fetchStudents.js

---
+++ VLISC901-estudiantes/src/main/resources/gestion901/fetchStudents.js
@@ -0,0 +1,24 @@
+const estudiante = {
+    nombre: "Ismael",
+    apellido: "Ramirez",
+    idCurso: 10
+}
+fetch('http://localhost:8585/api/univer/estudiante/save', {
+    method: "POST",
+    body: JSON.stringify(estudiante),
+    headers: {"Content-Type": "application/json"}
+})
+.then(response => {
+    if(response.ok && response.status===201){
+        console.log("Dentro de la primera Promesa")
+        return response.json()
+    }else{
+        throw Error(response.statusText)
+    }
+})
+.then(json=>{
+    console.log("segunda promesa")
+    console.log(json);
+    return json;
+})
+.catch(error => {console.log(error)})
