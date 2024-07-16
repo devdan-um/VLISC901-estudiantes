60283409557be19378cc968fc85b56451db56c10
goosvin <leonkennedy498@gmail.com>
2024-07-15 22:20:37 -0600

    actividad en clase 15/07

---
+++ VLISC901-estudiantes-feature/src/main/resources/fetch/fetchAlumnos.js
@@ -0,0 +1,24 @@
+const estudiante = {
+    nombre: "Jose",
+    apellido: "Condes",
+    idCurso: 3
+}
+fetch('http://localhost:8585/api/univer/estudiante/save', {
+    method: "POST",
+    body: JSON.stringify(estudiante),
+    headers: {"Content-Type": "application/json"}
+})
+.then(response => {
+    if(response.ok && response.status===201){
+        console.log("Dentro de la primera Promesa")
+        return response.json()
+    }else{
+        throw Error(response.statusText)
+    }
+})
+.then(json=>{
+    console.log("segunda promesa")
+    console.log(json);
+    return json;
+})
+.catch(error => {console.log(error)})
